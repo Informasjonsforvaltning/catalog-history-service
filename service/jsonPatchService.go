18a578ed1dbcdab4fc19987a5f79e98a1c7c737c
Daniel Nilsen <daniel.1.nilsen@baerum.kommune.no>
2023-01-09 08:17:22 +0100

    removed func, add later

--- service/jsonPatchService.go
+++ service/jsonPatchService.go
@@ -1,12 +1,7 @@
 package service
 
 import (
-	"context"
-	"encoding/json"
-
 	"github.com/Informasjonsforvaltning/catalog-history-service/config/connection"
-	"github.com/Informasjonsforvaltning/catalog-history-service/model"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,36 +14,4 @@ type JsonPatchService struct {
 func InitService() *JsonPatchService {
 	service := JsonPatchService{collection: connection.MongoCollection()}
 	return &service
-}
-
-// ApplyJSONPatch applies a JSON Patch to a mock JSON document stored in a MongoDB collection
-func (service *JsonPatchService) ApplyJSONPatch(ctx context.Context, client *mongo.Client) error {
-	// create a JSON Patch document to update the "title" field of the mock JSON document
-	patch, err := json.Marshal([]struct {
-		Op    string `json:"op"`
-		Path  string `json:"path"`
-		Value string `json:"value"`
-	}{{
-		Op:    "replace",
-		Path:  "/begreper",
-		Value: "Updated Begrep",
-	}})
-	if err != nil {
-		return err
-	}
-
-	// apply the JSON Patch document to the mock JSON document
-	var updatedBegrep model.Begrep
-	err = json.Unmarshal(patch, &updatedBegrep)
-	if err != nil {
-		return err
-	}
-
-	// update the stored document in the collection with the patched document
-	_, err = service.collection.UpdateOne(ctx, bson.M{"def": "someNewDef"}, bson.M{"$set": updatedBegrep})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
