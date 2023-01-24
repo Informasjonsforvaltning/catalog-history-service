b60249814a86280d86d1a76b2c76298885610341
Daniel Nilsen <daniel.1.nilsen@baerum.kommune.no>
2023-01-24 16:54:36 +0100

    added tests

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,35 +1,46 @@
 package test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/Informasjonsforvaltning/catalog-history-service/handlers"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/Informasjonsforvaltning/catalog-history-service/config"
 )
 
-func TestMain(m *testing.M) {
-	MongoContainerRunner(m)
+func TestGetAllHandler(t *testing.T) {
+	// Create a new Gin engine
+	r := gin.Default()
+	r.GET("/concepts", handlers.GetAllHandler())
+
+	// Create a new request and response recorder
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/concepts", nil)
+	r.ServeHTTP(w, req)
+
+	// Assert the response code and response body
+	assert.Equal(t, http.StatusOK, w.Code)
+	var concepts []interface{}
+	json.Unmarshal(w.Body.Bytes(), &concepts)
+	assert.NotEmpty(t, concepts)
 }
 
-func TestPingRoute(t *testing.T) {
-	router := config.SetupRouter()
+func TestGetUpdateHandler(t *testing.T) {
+	// Create a new Gin engine
+	r := gin.Default()
+	r.GET("/concepts/:conceptId", handlers.GetUpdateHandler())
 
+	// Create a new request and response recorder
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ping", nil)
-	router.ServeHTTP(w, req)
+	req, _ := http.NewRequest("GET", "/concepts/123", nil)
+	r.ServeHTTP(w, req)
 
+	// Assert the response code and response body
 	assert.Equal(t, http.StatusOK, w.Code)
-}
-
-func TestReadyRoute(t *testing.T) {
-	router := config.SetupRouter()
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ready", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusOK, w.Code)
+	var concept interface{}
+	json.Unmarshal(w.Body.Bytes(), &concept)
+	assert.NotNil(t, concept)
 }
