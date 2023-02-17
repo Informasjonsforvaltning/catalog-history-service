cc1ff7525402848914a32e56a55177c55c3e6478
Daniel Nilsen <daniel.1.nilsen@baerum.kommune.no>
2023-02-17 08:50:08 +0100

    failed tests

--- test/create_update_test.go
+++ test/create_update_test.go
@@ -18,7 +18,7 @@ func TestCreateUpdate(t *testing.T) {
 	w := httptest.NewRecorder()
 	toBeCreated := model.UpdatePayload{
 		Person: model.Person{
-			ID:    "123456789",
+			ID:    "123",
 			Email: "example@example.com",
 			Name:  "Ola Nordmann",
 		},
@@ -36,14 +36,11 @@ func TestCreateUpdate(t *testing.T) {
 	}
 
 	body, _ := json.Marshal(toBeCreated)
-	req, _ := http.NewRequest("POST", "/concepts/123456789", bytes.NewBuffer(body))
+	req, _ := http.NewRequest("POST", "/concepts/123456789/updates", bytes.NewBuffer(body))
 	router.ServeHTTP(w, req)
-	location, _ := w.Result().Location()
 
 	//assert.NotNil(t, location)
 	assert.Equal(t, http.StatusCreated, w.Code)
-req, _ := http.NewRequest("POST", "/concepts/123456789/updates", bytes.NewBuffer(body))
-	req, _ = http.NewRequest("GET", location.Path, nil)
 
 	var newUpdate model.Update
 	json.Unmarshal(w.Body.Bytes(), &newUpdate)
--- test/get_test.go
+++ test/get_test.go
@@ -15,9 +15,8 @@ func TestGetConcepts(t *testing.T) {
 	router := config.SetupRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/concepts/123456789", nil)
+	req, _ := http.NewRequest("GET", "/concepts/123456789/updates", nil)
 	router.ServeHTTP(w, req)
-req, _ := http.NewRequest("GET", "/concepts/123456789/updates", nil)
 	assert.Equal(t, http.StatusOK, w.Code)
 
 	var actualResponse []model.Update
