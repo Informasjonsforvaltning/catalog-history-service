38b88997ec5a588557f2a832410844a68fe5ddb1
Daniel Nilsen <daniel.1.nilsen@baerum.kommune.no>
2023-02-20 13:45:39 +0100

    Added more tests

--- test/get_test.go
+++ test/get_test.go
@@ -25,3 +25,18 @@ func TestGetConcepts(t *testing.T) {
 	assert.Nil(t, err)
 	assert.True(t, len(actualResponse.Updates) > 0)
 }
+
+func TestGetConceptUpdatesWithPagination(t *testing.T) {
+	router := config.SetupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/concepts/123456789/updates?page=1&size=2", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var actualResponse model.Updates
+	err := json.Unmarshal(w.Body.Bytes(), &actualResponse)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(actualResponse.Updates))
+}
