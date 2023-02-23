23a56f8e180e4a754e3618fbd3f21ebf74d3e93c
Daniel Nilsen <daniel.1.nilsen@baerum.kommune.no>
2023-02-23 09:17:42 +0100

    added tests

---
+++ test/sorting_order_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Informasjonsforvaltning/catalog-history-service/config"
+	"github.com/Informasjonsforvaltning/catalog-history-service/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetConceptUpdatesWithPaginationAndSorting(t *testing.T) {
+	router := config.SetupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/concepts/123456789/updates?page=1&size=2&sort_by=datetime&sort_order=desc", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var actualResponse model.Updates
+	err := json.Unmarshal(w.Body.Bytes(), &actualResponse)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(actualResponse.Updates))
+
+	// Check that updates are returned in descending order by date
+	assert.True(t, actualResponse.Updates[0].DateTime.After(actualResponse.Updates[1].DateTime))
+}
